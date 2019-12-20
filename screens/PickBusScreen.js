aa0f354d657b010c9c295e86f8f91abc6d95e082
iamstr <asatar1994@gmail.com>
2019-12-20 09:17:57 +0300

    border fixed

--- screens/PickBusScreen.js
+++ screens/PickBusScreen.js
@@ -186,7 +186,8 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
 
-    elevation: 5
+    elevation: 5,
+    borderRadius: 15
   },
   rounded: { borderRadius: 25 }
 });
